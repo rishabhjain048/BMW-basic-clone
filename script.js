0559afdf4f74a5f512ae970bf3915d0a4acce654
Rishabhjain048 <92505685+rishabhjain048@users.noreply.github.com>
2024-02-11 01:12:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('.main'),
+    smooth: true
+});
+
+
+// gsap.from(".p1 background-image",{
+//     x:-200,
+// })
+
+// gsap.from(" .p1 .d1",{
+//     y:-100,
+//     delay:1,
+//     duration:2,
+//     opacity:0
+// })
